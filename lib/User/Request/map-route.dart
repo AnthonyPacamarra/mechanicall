39eb81b5bbe49e7dd5abfc2c984ab4e66b91e6ff
Anthony_Pacamarra <anthonygwapopacamarra@gmail.com>
2023-11-23 12:58:36 +0000

    request help

---
+++ lib/User/Request/map-route.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'map-route2.dart';
+
+class Route1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Image.asset(
+            'assets/au-route1.png',
+            fit: BoxFit.cover,
+            width: double.infinity,
+            height: double.infinity,
+          ),
+          Positioned.fill(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(height: 167),
+                Container(
+                  width: 455,
+                  height: 455,
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        top: 60,
+                        left: 59,
+                        child: GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => Route2()));
+                          },
+                          child: Image.asset(
+                            'assets/au-mechanic.png',
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return Route2();
+                          },
+                        );
+                      },
+                      child: Container(
+                        width: 59,
+                        height: 59,
+                        decoration: ShapeDecoration(
+                          color: Color(0xFFBD632F),
+                          shape: OvalBorder(),
+                        ),
+                        child: Image.asset(
+                          'assets/au-check.png', // Adjust height as needed
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 21),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
